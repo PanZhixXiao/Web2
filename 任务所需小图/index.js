a9c8a4a9b10b69a34f013084ca9f0f0444c234d5
PanZhixXiao <1324961167@qq.com>
2018-12-31 14:40:46 +0800

    text

---
+++ 任务所需小图/index.js
@@ -0,0 +1,51 @@
+window.onload = function(){
+	var cover = document.getElementsByClassName('top')[0];
+	window.onscroll = function(){
+		var st = document.documentElement.scrollTop || document.body.scrollTop;
+		if(st>150){
+			cover.style.position = 'fixed';
+			cover.style.zIndex = 10000;
+			cover.style.marginLeft = "42px";
+		}else{
+			cover.style.position = 'static';
+		}
+	}
+}
+
+
+function getStyle(obj, attr){
+	if(obj.currentStyle){
+		return obj.currentStyle[attr];
+	} else {
+		return getComputedStyle(obj, null)[attr];
+	}
+}
+function animate(obj,json,callback){
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		var isStop = true;
+		for(var attr in json){
+			var now = 0;
+			if(attr == 'opacity'){
+				now = parseInt(getStyle(obj,attr)*100);
+			}else{
+				now = parseInt(getStyle(obj,attr));
+			}
+			var speed = (json[attr] - now) / 8;
+			speed = speed>0?Math.ceil(speed):Math.floor(speed);
+			var cur = now + speed;
+			if(attr == 'opacity'){
+				obj.style[attr] = cur / 100;
+			}else{
+				obj.style[attr] = cur + 'px';
+			}
+			if(json[attr] !== cur){
+				isStop = false;
+			}
+		}
+		if(isStop){
+			clearInterval(obj.timer);
+			callback&&callback();
+		}
+	}, 30)
+}
